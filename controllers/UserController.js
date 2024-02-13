038401367ad414e75660059f6186f7be1d4f8f21
WESLEY DOS ANJOS <wesleyanjosprogramming@outlook.com.br>
2024-02-13 17:32:08 -0300

    lendo arquivo de imagen

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,8 +9,28 @@ class UserController
     onSubmit() {
         this.formEl.addEventListener('submit', event => {
             event.preventDefault()
-            this.addLine(this.getValues())
+            let values = this.getValues()
+            values.photo = ''
+            this.getPhoto((content) => {
+                values.photo = content
+                this.addLine(values)
+            })
+            this.addLine(values)
         })
+    }
+
+    getPhoto(callback) {
+        let fileReader = new FileReader()
+        let elements = [...this.formEl.elements].filter(item => {
+            if (item.name === 'photo') {
+                return item
+            }
+        })
+        let file = elements[0].files[0]
+        fileReader.onload = () => {
+            callback(fileReader.result)
+        }
+        fileReader.readAsDataURL(file)
     }
 
     getValues() {
@@ -40,7 +60,7 @@ class UserController
     addLine(dataUser) {
         this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
