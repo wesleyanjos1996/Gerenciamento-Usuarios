9b2fcadd7deda68298088890d949eb29cfa9a3c1
WESLEY DOS ANJOS <wesleyanjosprogramming@outlook.com.br>
2024-02-13 10:56:48 -0300

    inserindo dados na talela

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
-var name = document.querySelector('#exampleInputName')
-var gender = document.querySelectorAll('#form-user-create [name=gender]:checked')
-var birth = document.querySelector('#exampleInputBirth')
-var country = document.querySelector('#exampleInputCountry')
-var email = document.querySelector('#exampleInputEmail1')
-var password = document.querySelector('#exampleInputPassword1')
-var photo = document.querySelector('#exampleInputFile')
-var admin = document.querySelector('#admin')
+// var name = document.querySelector('#exampleInputName')
+// var gender = document.querySelectorAll('#form-user-create [name=gender]:checked')
+// var birth = document.querySelector('#exampleInputBirth')
+// var country = document.querySelector('#exampleInputCountry')
+// var email = document.querySelector('#exampleInputEmail1')
+// var password = document.querySelector('#exampleInputPassword1')
+// var photo = document.querySelector('#exampleInputFile')
+// var admin = document.querySelector('#admin')
 
 var fields = document.querySelectorAll('#form-user-create [name]')
-var user = []
+var user = {}
+
+function addLine(dataUser) {
+    var tr = document.createElement('tr')
+    tr.innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `
+    document.getElementById('table-users').appendChild(tr)
+}
 
 document.getElementById('form-user-create').addEventListener('submit', function(event) {
     event.preventDefault()
     fields.forEach(function(field, index) {
         if (field.name == 'gender') {
             if (field.checked) {
-                user.gender
+                user[field.name] = field.value
             }
         } else {
             user[field.name] = field.value
         }
     })
-    console.log(user)
+    addLine(user)
 })
