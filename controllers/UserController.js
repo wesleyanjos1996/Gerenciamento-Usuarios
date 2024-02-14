11c43a44c29c707a0f637c49d25bc9611a706f9d
WESLEY DOS ANJOS <wesleyanjosprogramming@outlook.com.br>
2024-02-14 10:28:31 -0300

    ajuste no formulario pra editar usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,13 @@ class UserController
         this.formEl = document.getElementById(formId)
         this.tableEl = document.getElementById(tableId)
         this.onSubmit()
+        this.onEditCancel()
+    }
+
+    onEditCancel() {
+        document.querySelector('#box-user-update .btn-cancel').addEventListener('click', e => {
+            this.showPanelCreate()
+        })
     }
 
     onSubmit() {
@@ -94,13 +101,27 @@ class UserController
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-primary btn-xs btn-flat btn-edit">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         </tr>
         `
+        tr.querySelector('.btn-edit').addEventListener('click', e => {
+            console.log(JSON.parse(tr.dataset.user))
+            this.showPanelUpdate()
+        })
         this.tableEl.appendChild(tr)
         this.updateCount()
+    }
+
+    showPanelCreate() {
+        document.querySelector('#box-user-create').style.display = 'block'
+        document.querySelector('#box-user-update').style.display = 'none'
+    }
+
+    showPanelUpdate() {
+        document.querySelector('#box-user-create').style.display = 'none'
+        document.querySelector('#box-user-update').style.display = 'block'
     }
     
     updateCount() {
